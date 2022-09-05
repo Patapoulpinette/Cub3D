8995794b68cc87f9e4c552db9a3d7ef30c5faa3c
Delphine Bouron <dbouron@student.42lyon.fr>
2022-09-05 12:24:15 +0200

    Raycasting algo in progress - calculating all different positions

--- sources/initializing.c
+++ sources/initializing.c
@@ -6,7 +6,7 @@
 /*   By: dbouron <dbouron@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 10:58:07 by dbouron           #+#    #+#             */
-/*   Updated: 2022/08/25 11:36:33 by dbouron          ###   ########.fr       */
+/*   Updated: 2022/09/05 11:34:50 by dbouron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	init_raycasting_values(t_raycasting *raycasting)
 {
-	raycasting->player_x = 22;
-	raycasting->player_y = 12;
-	raycasting->direction_x = -1;
-	raycasting->direction_y = 0;
-	raycasting->camera_x = 0;
-	raycasting->camera_y = 0.66;
-	raycasting->frame_time = 0;
-	raycasting->old_frame_time = 0;
+	raycasting->player_x = 22;//posX
+	raycasting->player_y = 12;//posY
+	raycasting->direction_x = -1;//dirX
+	raycasting->direction_y = 0;//dirY
+	raycasting->camera_plane_x = 0;//planeX
+	raycasting->camera_plane_y = 0.66;//planeY
+	raycasting->frame_time = 0;//time
+	raycasting->old_frame_time = 0;//oldTime
 }
--- sources/raycasting.c
+++ sources/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: dbouron <dbouron@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 14:02:13 by dbouron           #+#    #+#             */
-/*   Updated: 2022/06/02 11:22:57 by dbouron          ###   ########.fr       */
+/*   Updated: 2022/09/05 12:16:01 by dbouron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,41 @@ double	degree_to_radian(int degree)
 	return (degree * M_PI / 180);
 }
 
-double pythagoras(int a, int b)
+double	pythagoras(int a, int b)
 {
 	return (sqrt(pow(a, 2) + pow(b, 2)));
 }
 
 void	raycasting_algo(t_image *image, t_raycasting *raycasting)
 {
+	int		x;
+	double	camera_x;
+	double	ray_x;
+	double	ray_y;
+	int		map_x;
+	int		map_y;
+	double	side_distance_x;
+	double	side_distance_y;
+	double	delta_distance_x;
+	double	delta_distance_y;
 
+	x = 0;
+	while (x < SCREEN_WIDTH)
+	{
+		//calculate ray position and direction
+		camera_x = 2 * x / (double)SCREEN_WIDTH - 1;//x coords in camera space
+		ray_x = raycasting->direction_x + raycasting->camera_plane_x * camera_x;
+		ray_y = raycasting->direction_y + raycasting->camera_plane_y * camera_x;
+
+		//which box of the map we're in
+		map_x = (int)raycasting->player_x;
+		map_y = (int)raycasting->player_y;
+
+		//length of ray from current position to next x or y-side
+		//length of ray from x or y-side to next x or y-side
+		delta_distance_x = ;
+		delta_distance_y = ;
+
+		x++;
+	}
 }
